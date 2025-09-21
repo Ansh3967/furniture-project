c348bfed08de281d3fcefc068bb72301ea941ff2
Dmouse1101 <anandaniansh@gmail.com>
2025-09-21 11:19:52 +0530

    item route

--- backend/src/routes/item.route.js
+++ backend/src/routes/item.route.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import Item from "../models/item.model.js";
 
 const itemRouter = Router();
 
-itemRouter.post("/add", () => {
-  console.log("hello bhen chodo");
+itemRouter.post("/add", async (req, res) => {
+  const item = new Item({
+    itemId: "1234",
+  });
+
+  await item.save();
+  res.status(201).send("gand");
 });
 
 export default itemRouter;
